6e7567ebaedb6c5ae10c7ec84f5ac4415d31ce79
nouman <noumanisrar786@gmail.com>
2025-10-17 13:54:31 +0500

    fixed version issue

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ let campaignStatus = 'not_started';
 async function startSock() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys');
 
+    const version = [2, 3000, 1027934701];
     return new Promise((resolve, reject) => {
         sock = makeWASocket({
+            version,
             auth: state,
             // printQRInTerminal: true,
         });
