594e164f74e815cb8e3c331caf9b569c3bea3ed7
Muhammad Imran Israr <mimranisrar6@gmail.com>
2025-10-26 16:19:18 +0500

    agents can run their campaign

--- multiple-accounts.js
+++ multiple-accounts.js
@@ -215,9 +215,7 @@ async function startSockFor(sid) {
                 }
 
                 let senderNumber = sender.replace('@s.whatsapp.net', '');
-                console.log('====ADMIN NUMBERS====', ADMINS_NUMBERS);
-                console.log('=== SENDER NUMBER ===', senderNumber);
-                if (!ADMINS_NUMBERS.includes(senderNumber) || !AGENTS_NUMBERS.includes(senderNumber)) {
+                if (!ADMINS_NUMBERS.includes(senderNumber) && !AGENTS_NUMBERS.includes(senderNumber)) {
                     await sock.sendPresenceUpdate('paused', sender);
                     await sock.sendMessage(sender, { text: '❌ You are not authorized to start campaign.' });
                     return;
